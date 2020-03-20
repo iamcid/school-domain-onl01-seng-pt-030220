09489fb1f25fffb298fefea83f2743e472684399
Christopher Cid <iamcid17@gmail.com>
2020-03-20 04:01:29 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,13 +1,13 @@
 class School
-  attr_reader :name
+  attr_accessor :name :roster
   
   def initialize(name)
     @name = name
     @roster = {}
   end
   
-  def roster
-    @roster
-  end
+  def add_student(student_name, grade)
+    roster[grade] ||= []
+    roster[grade] << student_name
   
 end
